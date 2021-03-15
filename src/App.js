52a928b4bbb0e1d5349bafabeb1fd8ad68379cb4
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2021-03-15 08:22:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './component/home'
+import Detail from './component/detail'
+import Login from './component/login'
+import Register from './component/register'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Component } from 'react';
+class App extends Component {
 
-function App() {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <Router>
+      <Switch>
+      {/* <Route path="/detail">
+          <Detail />
+        </Route> */}
+        <Route path="/detail" component={Detail} />
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
+}
 }
 
 export default App;
